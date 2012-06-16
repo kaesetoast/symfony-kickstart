b75ca042c8220b014948e685b2f904948d24f1c8
kaesetoast <philipp@nowinski.de>
2012-06-16 23:39:12 +0200

    fosuser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Notan\UserBundle\NotanUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Notan/UserBundle/Entity/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Notan\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+        $this->email = $username;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+        $this->username = $email;
+    }
+
+}
